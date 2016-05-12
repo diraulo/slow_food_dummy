18e1f53c56c8d6ebbae9032e49bcf9f035edae0c
DanielitoAlbinsson <mr.daniel.albinsson@gmail.com>
2016-05-12 11:43:06 +0200

    added more acceptance tests

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -9,3 +9,33 @@ end
 Then(/^I should be on the registration page$/) do
   expect(current_path).to eq '/auth/login'
 end
+
+
+
+#user_log_in.feature
+Given(/^a User exists$/) do
+  user = User.new
+  user.username = "test"
+  user.password = "test"
+  user.save
+end
+
+Given(/^I am on the "([^"]*)" page$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I fill in "([^"]*)" with "([^"]*)"$/) do |arg1, arg2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I click on "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should be on the "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
---
+++ features/user_log_in.feature
@@ -0,0 +1,14 @@
+Feature: As a registered user
+So that i can sign in
+I would like to log in using my credentials
+
+  Background:
+  Given a User exists
+
+  Scenario: Allows user to log in
+  Given I am on the "Log In" page
+  Then I fill in "username" with "test"
+  And I fill in "password" with "test"
+  And I click on "submit"
+  Then I should be on the "home page"
+  And I should see "Succesfully logged in"
